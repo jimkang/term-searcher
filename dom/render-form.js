20f72a616fcd067d8fe2089df4b257adfdfb5c59
Jim Kang <jimkang@gmail.com>
2020-01-01 15:25:17 -0500

    Link to weblog and summaries.

--- dom/render-form.js
+++ dom/render-form.js
@@ -1,11 +1,11 @@
 var titleLabel = document.getElementById('title');
 var termInput = document.getElementById('term');
 
-function renderForm({ term, archiveName }) {
+function renderForm({ term, archiveName, contentBaseURL }) {
   if (term) {
     termInput.value = term;
   }
-  titleLabel.textContent = `Search ${archiveName} archive`;
+  titleLabel.innerHTML = `Search <a href="${contentBaseURL}">${archiveName}</a> archive`;
   document.title = `Search ${archiveName} archive`;
 }
 
