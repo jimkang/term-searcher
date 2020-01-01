aebff90f70eb5d5bd9318d109d0229956f5c1e34
Jim Kang <jimkang@gmail.com>
2020-01-01 15:04:31 -0500

    Fix result entry links.

--- dom/render-results.js
+++ dom/render-results.js
@@ -1,15 +1,22 @@
 var getHTMLFragmentFromCell = require('@jimkang/get-html-fragment-from-cell');
 var d3 = require('d3-selection');
-var curry = require('lodash.curry');
 
 var resultsRoot = d3.select('#results-root');
 
 function renderResults({ resultsContents, contentBaseURL }) {
-  var fragments = resultsContents.map(
-    curry(getHTMLFragmentFromCell)(`${contentBaseURL}/media`)
-  );
+  var fragments = resultsContents.map(curriedGetFragment);
   resultsRoot.selectAll('li').remove();
   resultsRoot.html(fragments.join('\n'));
+
+  function curriedGetFragment(cell) {
+    return getHTMLFragmentFromCell(
+      {
+        mediaDir: `${contentBaseURL}/media`,
+        baseDir: contentBaseURL
+      },
+      cell
+    );
+  }
 }
 
 module.exports = renderResults;
